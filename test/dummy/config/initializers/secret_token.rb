b5171ac82502d59cfb23206d6df82c3e3e64bd66
Dieterich Lawson <dieterich.lawson@gmail.com>
2013-03-06 14:03:54 -0600

    More cleanup

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'd12c82bf5fb8a2285c225fe1769f1ea2d4259325854a256640c72ef843eca6c923fff24600687e2965317641adaa409c73faa117b2d7e5c8c216f13f55b199d7'
--- test/pagination_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PaginationTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Pagination
-  end
-end
