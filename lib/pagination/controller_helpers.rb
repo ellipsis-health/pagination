1e7382fe4777a19c5c1ac13b239a52f04a7a1d78
Dieterich Lawson <dieterich.lawson@gmail.com>
2013-03-15 16:43:37 -0500

    Removing pagination_json wrapper

--- lib/pagination/controller_helpers.rb
+++ lib/pagination/controller_helpers.rb
@@ -1,34 +1,12 @@
 module Pagination
   module ControllerHelpers
-    module ClassMethods
-      def paginate_json *actions
-        actions.each do |action|
-          alias_method("orig_#{action}".to_sym, action)
-          define_method(action) do 
-            paginate_wrapper { send("orig_#{action}".to_sym) }
-          end
-        end
-      end
-    end
- 
-    def self.included base
-      base.extend ClassMethods
-    end
-     
+        
     def redirect_with_pagination?
       not params.include?(:per_page) or not params.include?(:page)
     end
 
     def paginate_filter
       redirect_to({per_page: '10', page: '1'}.merge(params)) and return if redirect_with_pagination?
-    end
-
-    def paginate_wrapper
-      paginate_filter
-      @results = yield.paginate(:per_page => params[:per_page], :page => params[:page])
-      respond_to do |format|
-        format.json {render json: @results}
-      end
     end
   end
 end
